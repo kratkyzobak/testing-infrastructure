9221c1f837bd9092969a68609d21483f0bafce6e
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2022-10-13 21:50:56 +0200

    fix: Set correct retention for Azure Log Analytics (#18)

--- modules/azure/monitor-stack/main.tf
+++ modules/azure/monitor-stack/main.tf
@@ -16,7 +16,7 @@ resource "azurerm_log_analytics_workspace" "workspace" {
   location            = data.azurerm_resource_group.rg.location
   resource_group_name = data.azurerm_resource_group.rg.name
   sku                 = "PerGB2018"
-  retention_in_days   = 7
+  retention_in_days   = 30
 }
 
 resource "azurerm_application_insights" "insights" {
