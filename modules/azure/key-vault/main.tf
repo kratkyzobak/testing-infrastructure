d9d5206c192cc092f9cd28b890895cee7f43eb8e
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2022-10-15 02:53:14 +0200

    refactor modules (#28)

--- modules/azure/key-vault/main.tf
+++ modules/azure/key-vault/main.tf
@@ -26,19 +26,11 @@ resource "azurerm_key_vault" "vault" {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
 
-    key_permissions = [
-      "get",
-    ]
-
     secret_permissions = [
-      "get",
-      "set",
-      "list",
-      "delete"
-    ]
-
-    storage_permissions = [
-      "get",
+      "Get",
+      "Set",
+      "List",
+      "Delete"
     ]
   }
 
