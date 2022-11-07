893f4d20555b1b757fa4ab9cdc7162e33a5c9797
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2022-11-08 00:04:40 +0100

    fix typo with arm template (#41)

--- modules/azure/aks/main.tf
+++ modules/azure/aks/main.tf
@@ -38,7 +38,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
 # Terraform doesn't support MSI federation, replace this once it does
 resource "azurerm_resource_group_template_deployment" "msi_federation" {
-  name                = "msi_federation"
+  name                = "msi_federation-${var.cluster_name}-${var.workload_identity_applications[count.index].name}"
   count               = length(var.workload_identity_applications)
   resource_group_name = data.azurerm_resource_group.rg.name
 
