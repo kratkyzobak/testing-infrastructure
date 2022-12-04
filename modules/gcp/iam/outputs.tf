58178d85cca3dd98215a193bcd725f5616c83868
Jorge Turrado Ferrero <Jorge_turrado@hotmail.es>
2022-12-04 03:39:44 +0100

    decode sa secret (#70)

--- modules/gcp/iam/outputs.tf
+++ modules/gcp/iam/outputs.tf
@@ -1,5 +1,5 @@
 output "e2e_user_credentials" {
-  value = google_service_account_key.credentials.private_key
+  value = base64decode(google_service_account_key.credentials.private_key)
 }
 
 output "e2e_user_email" {
